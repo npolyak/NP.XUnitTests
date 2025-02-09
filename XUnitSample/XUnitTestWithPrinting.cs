cc805ed8fe64a894b6ca94024a275c5a80a1a206
npolyak <nick.polyak@verizon.net>
2025-02-08 22:55:48 -0500

    added comments

---
+++ XUnitSample/XUnitTestWithPrinting.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+
+namespace XUnitSample;
+
+public class XUnitTestWithPrinting
+{
+    private ITestOutputHelper _xunitOutput;
+
+    /// <summary>
+    /// pass a test console writer for the test 
+    /// (XUnit will provide the correct value itself)
+    /// </summary>
+    /// <param name="xunitOutput"></param>
+    public XUnitTestWithPrinting(ITestOutputHelper xunitOutput)
+    {
+        _xunitOutput = xunitOutput;
+    }
+
+    [Fact]
+    public void TestSplit()
+    {
+        string str = "Hello World";
+        string[] split = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        Assert.True(split.SequenceEqual(["Hello", "World"]));
+
+        foreach (string part in split)
+        {
+            // print each part to Test Console
+            _xunitOutput.WriteLine(part);
+        }
+    }
+}
---
+++ XUnitSample/XUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace XUnitSample;
+
+public class XUnitTests
+{
+    // fact sample
+    [Fact]
+    public void TestSplit()
+    {
+        string str = "Hello World";
+        string[] split = str.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        // assert that the split is a collection of two strings:
+        // "Hello" and "World"
+        Assert.True(split.SequenceEqual(["Hello", "World"]));
+    }
+
+    /// <summary>
+    /// Run TestSplits(...) methods for every [InlineData] 
+    /// providing various arguments.
+    /// </summary>
+    [Theory]
+    [InlineData("Hello", new string[] { "Hello" })]
+    [InlineData("Hello World", new string[] { "Hello", "World" })]
+    [InlineData("Hello Great World", new string[] { "Hello", "Great", "World" })]
+    public void TestSplits(string strToSplit, IEnumerable<string> expectedSplit)
+    {
+        string[] split = strToSplit.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        Assert.True(split.SequenceEqual(expectedSplit));
+    }
+}
